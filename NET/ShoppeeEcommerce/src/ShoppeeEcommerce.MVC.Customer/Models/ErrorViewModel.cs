a68d32227e6a01bd3a5615759f48efba50e926b7
Nguyễn Thái Hòa <hoa41300@gmail.com>
2026-04-15 13:23:45 +0700

    setup: Create .NET solution and projects
    Create .NET solution and add core projects, consists of:
    - Shared ViewModels class library.
    - ASP .NET Core Web API for backend.
    - ASP .NET Core MVC for customer frontend.
    Also, add other files such as .gitignore,...

---
+++ NET/ShoppeeEcommerce/src/ShoppeeEcommerce.MVC.Customer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoppeeEcommerce.MVC.Customer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
