ceeaf9995a16f31e6a09e03501cbf04fae3badeb
Nguyễn Thái Hòa <hoa41300@gmail.com>
2026-05-04 18:30:16 +0700

    setup(customer): Setup necessary components for the Customer site
    Install NuGet packages, use Tailwind css and DaisyUI for styling. Edit page layout.

---
+++ NET/ShoppeeEcommerce/src/ShoppeeEcommerce.MVC.Customer/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppeeEcommerce.MVC.Customer.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = new List<string> { "Electronics", "Fashion", "Home & Garden", "Gadgets" };
+            return View(categories);
+        }
+    }
+}
